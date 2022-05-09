0947e548d2ae8ad6ef230ee6c37165a5a3c1f6c1
Juliette Cordor <grassojames5@gmail.com>
2022-05-09 13:48:55 +1000

    updated build script and added clipboard fn

--- build.rs
+++ build.rs
@@ -1,19 +1,38 @@
+use std::{env::current_dir, path::PathBuf};
+
+use regex::Regex;
+
+fn read_dir(dir: PathBuf) -> Vec<PathBuf> {
+    let r = Regex::new(r"\.(c|h){1,2}(pp)?$").unwrap();
+    let mut files = Vec::<PathBuf>::new();
+
+    if let Ok(entries) = dir.read_dir() {
+        for entry in entries.flatten() {
+            let path = entry.path();
+            if entry.metadata().unwrap().is_dir() {
+                files.append(&mut read_dir(path));
+            } else if r.is_match(path.to_string_lossy().as_ref()) {
+                files.push(path);
+            }
+        }
+    }
+
+    files
+}
+
+const PLATFORM: &str = {
+    if !cfg!(target_os = "windows") {
+        "unix"
+    } else {
+        "windows"
+    }
+};
+
 fn main() {
+    let cwd = current_dir().unwrap();
+    let files = read_dir(cwd.join("src").join(PLATFORM));
+
     let mut builder = cc::Build::new();
 
-    if cfg!(target_os = "windows") {
-        builder
-            .files(vec![
-                "src/windows/admin/admin.c",
-                "src/windows/network/network.c",
-            ])
-            .compile("windows");
-    } else {
-        let libnm = pkg_config::probe_library("libnm").unwrap();
-
-        builder
-            .files(vec!["src/unix/admin/admin.c", "src/unix/network/network.c"])
-            .includes(libnm.include_paths)
-            .compile("unix");
-    }
+    builder.files(files).compile(PLATFORM);
 }
