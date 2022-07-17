2fea9ec7957e2544cf95f9c4154a75e0e09e383c
Juliette Cordor <grassojames5@gmail.com>
2022-07-18 08:53:42 +1000

    moved into seperate header file and removed using std

--- include/check-net.hpp
+++ include/check-net.hpp
@@ -1,12 +1,3 @@
-#include <iostream>
-#include <ObjBase.h> // include the base COM header
-#include <Netlistmgr.h>
-
-// Instruct linker to link to the required COM libraries
-#pragma comment(lib, "ole32.lib")
-
-using namespace std;
-
 enum class INTERNET_STATUS
 {
     CONNECTED,
@@ -15,49 +6,4 @@ enum class INTERNET_STATUS
     CONNECTION_ERROR
 };
 
-INTERNET_STATUS IsConnectedToInternet()
-{
-    INTERNET_STATUS connectedStatus = INTERNET_STATUS::CONNECTION_ERROR;
-    HRESULT hr = S_FALSE;
-
-    try
-    {
-        hr = CoInitialize(NULL);
-        if (SUCCEEDED(hr))
-        {
-            INetworkListManager *pNetworkListManager;
-            hr = CoCreateInstance(CLSID_NetworkListManager, NULL, CLSCTX_ALL, __uuidof(INetworkListManager), (LPVOID *)&pNetworkListManager);
-            if (SUCCEEDED(hr))
-            {
-                NLM_CONNECTIVITY nlmConnectivity = NLM_CONNECTIVITY::NLM_CONNECTIVITY_DISCONNECTED;
-                VARIANT_BOOL isConnected = VARIANT_FALSE;
-                hr = pNetworkListManager->get_IsConnectedToInternet(&isConnected);
-                if (SUCCEEDED(hr))
-                {
-                    if (isConnected == VARIANT_TRUE)
-                        connectedStatus = INTERNET_STATUS::CONNECTED;
-                    else
-                        connectedStatus = INTERNET_STATUS::DISCONNECTED;
-                }
-
-                if (isConnected == VARIANT_FALSE && SUCCEEDED(pNetworkListManager->GetConnectivity(&nlmConnectivity)))
-                {
-                    if (nlmConnectivity & (NLM_CONNECTIVITY_IPV4_LOCALNETWORK | NLM_CONNECTIVITY_IPV4_SUBNET | NLM_CONNECTIVITY_IPV6_LOCALNETWORK | NLM_CONNECTIVITY_IPV6_SUBNET))
-                    {
-                        connectedStatus = INTERNET_STATUS::CONNECTED_TO_LOCAL;
-                    }
-                }
-
-                pNetworkListManager->Release();
-            }
-        }
-
-        CoUninitialize();
-    }
-    catch (...)
-    {
-        connectedStatus = INTERNET_STATUS::CONNECTION_ERROR;
-    }
-
-    return connectedStatus;
-}
+INTERNET_STATUS IsConnectedToInternet();
