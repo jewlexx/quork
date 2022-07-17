06ed4305f899a0dc86248c1696d145754c3f4c69
Juliette Cordor <grassojames5@gmail.com>
2022-07-18 09:08:12 +1000

    added docs

--- src/windows/network.rs
+++ src/windows/network.rs
@@ -17,10 +17,33 @@ mod ffi {
 
 pub use ffi::NetworkStatus;
 
+/// Network Status
+///
+/// NOTE: This function **DOES NOT** detect if the user has the ability to access the internet. It only checks if they are connected to a network, which may not be connected to the internet
+///
+/// Will return the status of connection as [`NetworkStatus`]
 pub fn network_status() -> NetworkStatus {
     ffi::IsConnectedToNetwork()
 }
 
+/// Network Detection
+///
+/// NOTE: This function **DOES NOT** detect if the user has the ability to access the internet. It only checks if they are connected to a network, which may not be connected to the internet
+///
+/// # Example
+///
+/// ```
+/// # use quork::network::is_connected_to_network;
+/// # fn main() {
+/// let connected = is_connected_to_network();
+///
+/// if connected {
+///     println!("We are connected to the internet");
+/// } else {
+///     println!("We are not connected to the internet");
+/// }
+/// # }
+/// ```
 pub fn is_connected_to_network() -> bool {
     let status = network_status();
 
