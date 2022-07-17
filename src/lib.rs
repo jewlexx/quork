7e63ea8524edd9307202ae5b65911bf45701ac5b
Juliette Cordor <grassojames5@gmail.com>
2022-07-18 09:09:40 +1000

    added crate level docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+#![warn(missing_docs)]
+
+//! A little crate that helps with interacting with the system
+//!
+//! The lower levels can be hard. So this crate is intended to help with that by abstracing the lower levels to high level code
+
 pub use quork_proc as macros;
 
 cfg_if::cfg_if! {
