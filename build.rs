9d445e1e39597b5a95e56f44551958886ee078f9
Juliette Cordor <grassojames5@gmail.com>
2022-07-18 09:16:08 +1000

    fixed build script

--- build.rs
+++ build.rs
@@ -5,5 +5,9 @@ fn main() {
         builder
             .file("src/windows/admin/admin.c")
             .compile("win_admin");
+
+        cxx_build::bridge("src/windows/network.rs")
+            .file("include/windows/check-net.hpp")
+            .compile("win_net");
     }
 }
