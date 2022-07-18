271dec6dd42b8a7a10b85782d89d12376a164b9b
Juliette Cordor <grassojames5@gmail.com>
2022-07-18 10:17:53 +1000

    fixed enum assignment

--- include/windows/network.hpp
+++ include/windows/network.hpp
@@ -2,10 +2,10 @@ namespace network
 {
     enum class INTERNET_STATUS
     {
-        CONNECTED,
-        DISCONNECTED,
-        CONNECTED_TO_LOCAL,
-        CONNECTION_ERROR
+        CONNECTED = 0,
+        DISCONNECTED = 1,
+        CONNECTED_TO_LOCAL = 2,
+        CONNECTION_ERROR = 3
     };
 
     INTERNET_STATUS IsConnectedToNetwork();
