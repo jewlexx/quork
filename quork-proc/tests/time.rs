1a8325c3f7abe47e6d77aa55a3fc230042c78c4f
Juliette Cordor <grassojames5@gmail.com>
2022-08-15 15:07:39 +1000

    added test

---
+++ quork-proc/tests/time.rs
@@ -0,0 +1,17 @@
+use quork_proc::time;
+
+#[test]
+#[time]
+fn test_time_maths() {
+    assert_eq!(2 + 2, 4);
+}
+
+#[test]
+fn test_time_unsafe() {
+    #[time]
+    unsafe fn unsafe_fn() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    unsafe { unsafe_fn() };
+}
