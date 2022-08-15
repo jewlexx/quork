057557c96f1698935afb54a5cd7f81d8d1c31301
Juliette Cordor <grassojames5@gmail.com>
2022-08-15 13:49:48 +1000

    fix

--- build.rs
+++ build.rs
@@ -6,11 +6,6 @@ fn main() {
             .file("src/windows/admin/admin.c")
             .compile("win_admin");
 
-        cxx_build::bridge("src/windows/network.rs")
-            .file("src/windows/network.cc")
-            .flag_if_supported("-std=c++14")
-            .compile("win_net");
-
         println!("cargo:rerun-if-changed=src/windows/admin/admin.c");
         println!("cargo:rerun-if-changed=src/windows/network.cc");
         println!("cargo:rerun-if-changed=include/windows/network.h");
