7eb2b6f16d802b6937e73a89a2abe587b9867bf0
Juliette Cordor <grassojames5@gmail.com>
2022-08-15 14:07:02 +1000

    fix

--- src/windows/admin/mod.rs
+++ src/windows/admin/mod.rs
@@ -1,8 +1,25 @@
+use winapi::{
+    ctypes::c_void,
+    shared::ntdef::NULL,
+    um::{
+        processthreadsapi::{GetCurrentProcess, OpenProcessToken},
+        winnt::TOKEN_QUERY,
+    },
+};
+
 extern "C" {
     fn IsElevated() -> u8;
 }
 
 /// Checks if the user is an administrator.
 pub fn is_admin() -> bool {
+    unsafe {
+        let process = GetCurrentProcess();
+
+        let mut htoken: *mut c_void = NULL;
+
+        let token = OpenProcessToken(process, TOKEN_QUERY, &mut htoken);
+    };
+
     unsafe { IsElevated() != 0 }
 }
