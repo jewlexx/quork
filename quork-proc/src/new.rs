48e32ac22fc592ac4d79ef8b929a9964c8a17e19
Juliette Cordor <grassojames5@gmail.com>
2022-10-12 09:33:01 +1100

    fix

--- quork-proc/src/new.rs
+++ quork-proc/src/new.rs
@@ -26,9 +26,9 @@ pub fn derive_new(ast: DeriveInput) -> TokenStream {
 
     quote! {
         impl #name {
-            pub fn new(#(args,)*) -> Self {
+            pub fn new(#(#args,)*) -> Self {
                 Self {
-                    #(con,)*
+                    #(#con,)*
                 }
             }
         }
