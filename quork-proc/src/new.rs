ada79333648dabfa27c011e2c183bf33c7a5b400
Juliette Cordor <grassojames5@gmail.com>
2022-10-12 09:25:30 +1100

    fix

--- quork-proc/src/new.rs
+++ quork-proc/src/new.rs
@@ -3,10 +3,18 @@ use syn::{Data, DeriveInput};
 
 pub fn derive_new(ast: DeriveInput) -> TokenStream {
     let name = &ast.ident;
-    let vars = match &ast.data {
-        Data::Struct(v) => &v.variants,
+    let fields = match &ast.data {
+        Data::Struct(v) => &v.fields,
         _ => return macro_error!("Can only be derived on a struct"),
     };
 
-    quote!()
+    quote! {
+        impl #name {
+            pub fn new(#fields) -> Self {
+                Self {
+                    #fields
+                }
+            }
+        }
+    }
 }
