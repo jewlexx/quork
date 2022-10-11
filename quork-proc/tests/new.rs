5068f7b9fd61decb4ab221954c6b4a536fca74ff
Juliette Cordor <grassojames5@gmail.com>
2022-10-12 09:35:41 +1100

    fix

--- quork-proc/tests/new.rs
+++ quork-proc/tests/new.rs
@@ -2,6 +2,14 @@ use quork_proc::New;
 
 #[derive(New)]
 struct TestImplNew {
-    a: i32,
-    b: i32,
+    pub a: i32,
+    pub b: i32,
+}
+
+#[test]
+fn test_new_impl() {
+    let s = TestImplNew::new(1, 2);
+
+    assert_eq!(s.a, 1);
+    assert_eq!(s.b, 1);
 }
