a6c0de200b569f4eb52ad9c498b41b5034f9e17e
Juliette Cordor <grassojames5@gmail.com>
2022-10-12 09:31:03 +1100

    fix

--- quork-proc/src/new.rs
+++ quork-proc/src/new.rs
@@ -1,5 +1,5 @@
 use proc_macro2::TokenStream;
-use syn::{Data, DeriveInput};
+use syn::{Data, DeriveInput, Ident};
 
 pub fn derive_new(ast: DeriveInput) -> TokenStream {
     let name = &ast.ident;
@@ -7,6 +7,21 @@ pub fn derive_new(ast: DeriveInput) -> TokenStream {
         Data::Struct(v) => &v.fields,
         _ => return macro_error!("Can only be derived on a struct"),
     };
+
+    let (con, args) = fields
+        .iter()
+        .enumerate()
+        .map(|(i, field)| {
+            let ident = field
+                .ident
+                .as_ref()
+                .map(|i| i.to_string())
+                .unwrap_or(i.to_string());
+            let ty = &field.ty;
+
+            (quote!(#ident), quote!(#ident: #ty))
+        })
+        .collect::<Vec<_>>();
 
     quote! {
         impl #name {
