86590f8eb7361e05d8870d9fb0c4d3165dfd511e
Juliette Cordor <grassojames5@gmail.com>
2022-10-12 09:42:46 +1100

    fix

--- quork-proc/tests/new.rs
+++ quork-proc/tests/new.rs
@@ -13,3 +13,11 @@ fn test_new_impl() {
     assert_eq!(s.a, 1);
     assert_eq!(s.b, 2);
 }
+
+const S: TestImplNew = TestImplNew::new(1, 2);
+
+#[test]
+fn test_new_const() {
+    assert_eq!(S.a, 1);
+    assert_eq!(S.b, 2);
+}
