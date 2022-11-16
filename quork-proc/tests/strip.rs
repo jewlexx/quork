27ce4cf885d16ce68fa57db1bb9bead35a19c856
Juliette Cordor <grassojames5@gmail.com>
2022-11-16 22:06:46 +1100

    fixes

--- quork-proc/tests/strip.rs
+++ quork-proc/tests/strip.rs
@@ -1,8 +1,56 @@
-fn test_multiline_js() {
-    let input = r#"
+use quork_proc::{lstrip_lines, rstrip_lines, strip_lines};
+
+const WHITESPACE: &str = "           ";
+
+fn test_multiline_both() {
+    let expected = r#"function foo() {
+return "bar";
+}
+"#;
+
+    let actual = strip_lines!(
+        r#"
         function foo() {
             return "bar";
         }
-    "#;
-    // let expected = r#"
+    "#
+    );
+
+    assert_eq!(actual, expected);
+}
+
+fn test_multiline_left() {
+    let expected = r#"function foo() {{
+let foo = "bar";
+        return foo;
+}}
+"#;
+
+    let actual = lstrip_lines!(
+        r#"
+        function foo() {
+            return "bar";
+        }
+    "#
+    );
+
+    assert_eq!(actual, expected);
+}
+
+fn test_multiline_right() {
+    let expected = r#"function foo() {{
+let foo = "bar";
+return foo;
+}}
+"#;
+
+    let actual = rstrip_lines!(
+        r#"
+        function foo() {
+            return "bar";
+        }
+    "#
+    );
+
+    assert_eq!(actual, expected);
 }
