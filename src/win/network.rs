b8314b1f44e4ff7b64146b9adc934aea257b5c82
Juliette Cordor <juliette@jewelexx.com>
2023-03-29 10:30:18 +1100

    fix

--- src/win/network.rs
+++ src/win/network.rs
@@ -5,7 +5,7 @@ use windows::Win32::Networking::NetworkListManager::INetworkListManager;
 pub unsafe fn get_manager() {
     let layout = {
         let size = std::mem::size_of::<INetworkListManager>();
-        let pointer =
-            std::mem::transmute::<_, *mut INetworkListManager>(std::mem::zeroed::<usize>());
+
+        std::mem::transmute::<_, *mut INetworkListManager>(std::mem::zeroed::<usize>())
     };
 }
