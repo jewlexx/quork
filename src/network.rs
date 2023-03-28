5db718e14de3457c355970d9e9a052c2d7f5c7d5
Juliette Cordor <juliette@jewelexx.com>
2023-03-29 10:21:56 +1100

    updates to network

--- src/network.rs
+++ src/network.rs
@@ -1,4 +1,14 @@
 //! Network helpers
 
 #[cfg(windows)]
-mod windows {}
+mod windows {
+    use windows::Win32::System::Com::{CoInitializeEx, CoUninitialize, COINIT_MULTITHREADED};
+
+    pub unsafe fn init_com() -> Result<(), windows::core::Error> {
+        CoInitializeEx(None, COINIT_MULTITHREADED)
+    }
+
+    pub unsafe fn uninit_com() {
+        CoUninitialize()
+    }
+}
