fcd5d2b413dafdbb49ac4ae1925ee4a85c5440d9
Juliette Cordor <juliette@jewelexx.com>
2023-03-29 10:30:05 +1100

    fixes

--- src/win/network.rs
+++ src/win/network.rs
@@ -1,1 +1,11 @@
 //! Network helpers
+
+use windows::Win32::Networking::NetworkListManager::INetworkListManager;
+
+pub unsafe fn get_manager() {
+    let layout = {
+        let size = std::mem::size_of::<INetworkListManager>();
+        let pointer =
+            std::mem::transmute::<_, *mut INetworkListManager>(std::mem::zeroed::<usize>());
+    };
+}
