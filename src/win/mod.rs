b6e01c6490552dcb27e44ba3b1d8a812f52670c0
Juliette Cordor <juliette@jewelexx.com>
2023-03-29 10:25:39 +1100

    fixes

--- src/win/mod.rs
+++ src/win/mod.rs
@@ -1,1 +1,25 @@
+use windows::Win32::System::Com::{CoInitializeEx, CoUninitialize, COINIT_MULTITHREADED};
+
+pub struct ComInit {
+    initialized: bool,
+}
+
+impl ComInit {
+    pub fn init() -> Self {
+        Self {
+            initialized: unsafe { Self::init_com() }.is_ok(),
+        }
+    }
+
+    unsafe fn init_com() -> Result<(), windows::core::Error> {
+        CoInitializeEx(None, COINIT_MULTITHREADED)
+    }
+}
+
+impl Drop for ComInit {
+    fn drop(&mut self) {
+        unsafe { CoUninitialize() }
+    }
+}
+
 pub mod network;
--- src/win/network.rs
+++ src/win/network.rs
@@ -1,14 +1,1 @@
 //! Network helpers
-
-#[cfg(windows)]
-mod windows {
-    use windows::Win32::System::Com::{CoInitializeEx, CoUninitialize, COINIT_MULTITHREADED};
-
-    pub unsafe fn init_com() -> Result<(), windows::core::Error> {
-        CoInitializeEx(None, COINIT_MULTITHREADED)
-    }
-
-    pub unsafe fn uninit_com() {
-        CoUninitialize()
-    }
-}
