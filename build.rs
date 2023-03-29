08115b87cddfeec5a68dc1befee7c7b3d3f7e457
Juliette Cordor <juliette@jewelexx.com>
2023-03-29 14:58:50 +1100

    added ole32 lib

--- build.rs
+++ build.rs
@@ -2,6 +2,8 @@ use cc::Build;
 
 fn main() {
     if cfg!(windows) {
+        println!("cargo:rustc-link-lib=ole32.lib");
+
         Build::new()
             .include("netlistmgr.h")
             .compile("netlistmgr.header");
