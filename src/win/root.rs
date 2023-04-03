6e98a418da2d01183d7e336ff01e05ea9a4adc61
Juliette Cordor <juliette@jewelexx.com>
2023-04-03 14:00:18 +1000

    fixes

--- src/win/root.rs
+++ src/win/root.rs
@@ -1,6 +1,9 @@
 //! Checks user root privelages
 
-use std::{ffi::c_void, mem};
+use std::{
+    ffi::c_void,
+    mem::{self, MaybeUninit},
+};
 
 use windows::Win32::{
     Foundation::{CloseHandle, INVALID_HANDLE_VALUE, TRUE},
@@ -14,17 +17,17 @@ pub fn is_elevated() -> bool {
         let mut token = INVALID_HANDLE_VALUE;
 
         let elevated = if OpenProcessToken(GetCurrentProcess(), TOKEN_QUERY, &mut token) == TRUE {
-            let mut elevation: TOKEN_ELEVATION = mem::zeroed();
+            let mut elevation: MaybeUninit<TOKEN_ELEVATION> = MaybeUninit::uninit();
             let mut size = mem::size_of::<TOKEN_ELEVATION>() as u32;
             if GetTokenInformation(
                 token,
                 TokenElevation,
-                Some(&mut elevation as *mut TOKEN_ELEVATION as *mut c_void),
+                Some(elevation.as_mut_ptr() as *mut c_void),
                 size,
                 &mut size,
             ) == TRUE
             {
-                elevation.TokenIsElevated != 0
+                elevation.assume_init().TokenIsElevated != 0
             } else {
                 false
             }
