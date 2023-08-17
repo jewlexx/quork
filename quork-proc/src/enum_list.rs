e3b0e7db0508bf26d0ca1648196d36a4f598705f
Juliette Cordor <juliette@jewelexx.com>
2023-08-17 10:22:33 +1000

    added trait ident

--- quork-proc/src/enum_list.rs
+++ quork-proc/src/enum_list.rs
@@ -1,6 +1,18 @@
-use proc_macro2::TokenStream;
+use proc_macro2::{Ident, Span, TokenStream};
 use syn::DeriveInput;
 
+use proc_macro_crate::{crate_name, FoundCrate};
+
 pub fn enum_list(ast: DeriveInput) -> TokenStream {
+    let found_crate = crate_name("quork").expect("quork is present in `Cargo.toml`");
+
+    let trait_ident = match found_crate {
+        FoundCrate::Itself => quote!(crate::traits::list::ListVariants),
+        FoundCrate::Name(name) => {
+            let ident = Ident::new(&name, Span::call_site());
+            quote!( #ident::traits::list::ListVariants )
+        }
+    };
+
     todo!()
 }
