96a08c79499dab82247ada7a0c91b278d6e59b8e
Juliette Cordor <me@cordor.dev>
2025-05-03 17:53:14 +1000

    removed std feature guards

--- src/traits/mod.rs
+++ src/traits/mod.rs
@@ -1,28 +1,16 @@
 //! Trait implementations
 
 pub mod flip;
+pub mod list;
 pub mod map;
 pub mod truncate;
-cfg_if::cfg_if! {
-    // TODO: Get these working without std crate (as much as possible)
-    if #[cfg(feature = "std")] {
-    }
-}
-
-pub mod list;
 pub mod truthy;
 
 /// Trait prelude imports
 pub mod prelude {
-    cfg_if::cfg_if! {
-        // TODO: Get these working without std crate (as much as possible)
-        if #[cfg(feature = "std")] {
-            pub use super::flip::*;
-            pub use super::truncate::*;
-            pub use super::map::*;
-        }
-    }
-
+    pub use super::flip::*;
     pub use super::list::*;
+    pub use super::map::*;
+    pub use super::truncate::*;
     pub use super::truthy::*;
 }
